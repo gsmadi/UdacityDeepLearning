6b0f6e352e5e9880ef7c53b2216d3b1c04e4a09e
Gabriel <gabriel.smadi@gmail.com>
2016-02-05 13:37:57 -0500

    assignment 1

---
+++ softmax.py
@@ -0,0 +1,23 @@
+"""Softmax."""
+
+
+import numpy as np
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    probs = np.exp(x)/np.sum(np.exp(x), axis=0)
+    return probs
+    # TODO: Compute and return softmax(x)
+
+scores = np.array([3.0, 1.0, 0.2])
+
+print(softmax(scores/10))
+print(np.sum(softmax(scores)))
+
+# Plot softmax curves
+import matplotlib.pyplot as plt
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
